c11b18b90563447342542b916b3e12656c5d8680
Michael Sjöberg <mail@michaelsjoeberg.com>
2024-12-30 01:51:36 +0700

    Update dscl.py

--- dscl.py
+++ dscl.py
@@ -1,3 +1,4 @@
+
 # MIT License
 
 # Copyright 2024 @asyncze (Michael Sjöberg)
@@ -20,3 +21,33 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+def parse(path) -> dict:
+    config = {}
+    with open(path, "r") as file:
+        content = file.readlines()
+        header = None
+        config_key = None
+        config_value = None
+
+        for line in content:
+            if line.strip() == "": continue             # skip empty lines
+            if line.strip().startswith("--"): continue  # skip line comment
+            
+            values = line.split(" ", 1)
+
+            if len(values) == 1:
+                header = values[0].strip().replace("[", "").replace("]", "")
+                config[str(header)] = {}
+            else:
+                config_key = values[0]
+                config_value = values[1].strip().replace("\"", "")
+
+                if "--" in config_value and not config_value.startswith("--"): config_value = config_value.split("--", 1)[0].strip()
+
+                if config_value == "true": config_value = True
+                elif config_value == "false": config_value = False
+                elif config_value.isdigit(): config_value = int(config_value)
+
+                if config_key != None and config_value != None: config[header][str(config_key)] = config_value
+
+    return config
